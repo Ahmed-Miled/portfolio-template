4b7278b8361d0bfa24a2950d317bdcadcc8ff0b8
Ahmed-Miled <ml967799@gmail.com>
2025-06-08 22:14:39 +0100

    deployment changes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
     mode: isDevelopment ? 'development' : 'production',
     entry: './src/index.jsx',
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, '../server/public'),
       filename: 'bundle.js',
       publicPath: '/',
       clean: true,
