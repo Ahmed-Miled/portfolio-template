88c1f3d2c722c8ffcf74b28956dcfe76f0006a1a
Ahmed-Miled <ml967799@gmail.com>
2025-06-11 21:27:44 +0100

    testing projects routers

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -5,6 +5,7 @@ import pool from '../db/index.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  /*
   try {
     const result = await pool.query('SELECT * FROM projects ORDER BY id DESC');
     res.json(result.rows);
@@ -12,6 +13,14 @@ router.get('/', async (req, res) => {
     console.error('Error fetching projects:', err);
     res.status(500).json({ error: 'Server error' });
   }
-});
+    */
 
+  try {
+    const result = await pool.query('SELECT * FROM projects');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('DB error:', err.message);
+    res.status(500).json({ error: 'Server error', detail: err.message });
+  }
+});
 export default router;
